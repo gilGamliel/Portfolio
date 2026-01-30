861b7667befb4bdc085f0165d7597f8feb9de448
gilgamliel <gilpsp3g@gmail.com>
2026-01-30 11:22:16 +0200

    check v1

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
-const repoName = "PortfolioGilGamliel"; // 👈 change this
-
 const nextConfig: NextConfig = {
-  output: "export",
-
   reactCompiler: true,
-
   images: {
     unoptimized: true,
   },
-
-  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
